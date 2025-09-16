92c2ccf5f4185d13b64bb3fdb6ca9ea3daa777aa
sharan <maguiret4@gmail.com>
2025-09-16 18:48:58 +0530

    changing something on app.py

---
+++ Webserver/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Sharan's CI/CD pipeline!"
+
+if __name__ == '__main__':
+    print("Starting Flask app...")
+    app.run(host='0.0.0.0', port=5000)
