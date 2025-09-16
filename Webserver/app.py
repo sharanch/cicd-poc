98354b085e30d2f7aeb5a06675be758af2728f43
sharan <maguiret4@gmail.com>
2025-09-16 21:18:23 +0530

    adding index.html

--- Webserver/app.py
+++ Webserver/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "index.html"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     print("Starting Flask app...")
